656137683f1a8e9191e3a5c8293038381c674c34
payalmujavadiya <Pmujavadiya@ibahealth.local>
2015-12-16 18:32:01 +0530

    registraion

--- register.php
+++ register.php
@@ -1,18 +1,22 @@
 <?php
 $servername="localhost";
-%dbname="mydb";
-$conn=new mysqli($servername,"","",$dbname)
+$dbname="my_db";
+$conn=new mysqli($servername,"root","",$dbname);
 if($conn->connect_error){
-die("Connection failed: " .$conn->connect_error)}
-$sql= "INSERT INTO employees VALUES($_POST["fname"],$_POST["lname"],$_POST["bday"],$_POST["email"],$_POST["desig"],$_POST["place"],$_POST["pwd"])";
+die("Connection failed: " .$conn->connect_error);}
+if(isset($_POST['submit'])){
+$sql="INSERT INTO employees(First_Name,Last_Name,Date_Of_Birth,Email,Designation,Place,Password,Gender,Contact_no)VALUES('$_POST[fname]','$_POST[lname]','$_POST[bday]','$_POST[email]','$_POST[desig]','$_POST[place]','$_POST[pwd]','$_POST[optradio]','$_POST[cno]')";
 if($conn->query($sql)==TRUE){
 echo "New Record created successfully";}
-else{
-echo "Eroor: " .$sql . "<br>" .$conn->error;
+else
+echo"error";
+
 }
+
+
+
 $conn->close();
 ?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
 <style>#box {
@@ -20,12 +24,6 @@ $conn->close();
     border: 2px solid #B8860B;
     padding: 20px; 
     }
-body {
-    background-color: lightblue;
-}
-
-
-
 </style>
   <title>Registration</title>
   <meta charset="utf-8">
@@ -35,6 +33,7 @@ body {
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
+
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -60,35 +59,38 @@ body {
 </nav>
 <div class="container">
 
-  <form id="register" role="form" class="col-sm-4">
+  <form id="register" role="form" class="col-sm-4" method="post" action="register.php">
 <fieldset id="box">
 <legend><b>Register</b></legend>
-      
-      <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter First Name">
+            <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter First Name">
 <br>
       <input type="text" class="form-control" id="lname" name="lname"placeholder="Enter Last Name">
 <br>
 <input type="date" class="form-control" id="bday" name="bday" >
-<br>
+
+<label class="radio-inline">
+      <input type="radio" value="M" name="optradio">Male
+    </label>
+    <label class="radio-inline">
+      <input type="radio" value="F" name="optradio">Female
+    </label>
+   <br>
       <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
    <br>
       <input type="text" class="form-control" id="desg" name="desig" placeholder="Enter Designation">
  <br>
-      
-      <input type="text" class="form-control" id="place" name="place" placeholder="Enter Your Place Of work">
-
+            <input type="text" class="form-control" id="place" name="place" placeholder="Enter Your Place Of work">
+<br>
+<input type="number" class="form-control" id="cno" name="cno" placeholder="Enter 10 digit Contact no">
     <br>
       <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter password">
 <br>
       <input type="password" class="form-control" id="cpwd" name="cpwd" placeholder="Enter Confirm password">
-
 <br>
-    <button type="submit" class="btn btn-default">Submit</button>
-</div>
+    <button type="submit" name ="submit"  class="btn btn-default">Submit</button>
+
 </fieldset>
   </form>
-
 </div>
-
 </body>
 </html>
